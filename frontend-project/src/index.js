ce03dcb1c9dfc81672f870036106b10830eb023b
KaoriIzutani <41350890+KaoriIzutani@users.noreply.github.com>
2021-06-03 19:05:00 -0700

    Adding components project

--- frontend-project/src/index.js
+++ frontend-project/src/index.js
@@ -4,17 +4,22 @@ import ReactDOM from 'react-dom';
 
 // 2. Create a React component
 /*Hint: Shortcut for comments: Alt + Shift + A. Common convention for 
-   creating readable code: Any time you have multi-line JSX, move your opening tag to 
-   the next line, indent once, then put the lines in parenthesis "( " is on the same
-  line as "return and ") " is on a new line after the "div" closing tag.*/ 
+   creating readable code: Any time you have multi-line JSX, move 
+   your opening tag (in this case, the "div" tag) to the next line, 
+   indent once, then put the lines in parenthesis "( " is on the 
+   same line as "return and  ")" is on a new line after the "div" 
+   closing tag.*/ 
 
 const App = function () {
+  const buttonText = 'Click me';
     return (
      <div> 
-      <label class="label" for="name"> Enter name </label>
+      <label className="label" htmlFor="name">
+       Enter name 
+      </label>
       <input id="name" type="text" />
       <button style={{backgroundColor: 'blue', color: 'white'}}> 
-      Submit 
+      {buttonText}
       </button> 
     </div>
   );
@@ -22,11 +27,13 @@ const App = function () {
 
 // 3. Take the React component and show it on the screen 
 ReactDOM.render(<App />, 
-    document.querySelector('#root') //This call summons the div id of "root" in the index.html
-    //file, which is in the "public" folder. Compatible with all web browsers.
+    document.querySelector('#root') /*This call summons the div id of 
+    "root" in the index.html file, which is in the "public" folder. 
+    Compatible with all web browsers.*/
 );
 
-/* Instead of function(), you can write arrow functions instead like this: 
+/* Instead of function(), you can write arrow functions instead like 
+this: 
 () => {
     return <div>Hi There!</div>;
 }; */
